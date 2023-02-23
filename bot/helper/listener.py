6b58ee002745298a804f4c858c0e5bb82a515e9e
Your Name <you@example.com>
2023-02-23 20:39:23 +0700

    add Stream Link button type video

--- bot/helper/listener.py
+++ bot/helper/listener.py
@@ -5,6 +5,7 @@ from os import walk, path as ospath
 from html import escape
 from aioshutil import move
 from asyncio import create_subprocess_exec, sleep, Event
+import base64
 
 from bot import Interval, aria2, DOWNLOAD_DIR, download_dict, download_dict_lock, LOGGER, DATABASE_URL, MAX_SPLIT_SIZE, config_dict, status_reply_dict_lock, user_data, non_queued_up, non_queued_dl, queued_up, queued_dl, queue_dict_lock
 from bot.helper.ext_utils.bot_utils import sync_to_async
@@ -304,8 +305,16 @@ class MirrorLeechListener:
                     share_url += '/'
                     buttons.ubutton("⚡ Index Link", share_url)
                 elif "video" in typ:
-                    INDEX_URLVIDEO = INDEX_URL.replace("0:/", "video/")
-                    share_urlvideo = f'{INDEX_URLVIDEO}/{url_path}'
+                    result_get_0 = INDEX_URL.split("/")[3]
+                    resultpath = '/' + result_get_0 + '/' + url_path
+                    encoded_string = base64.b64encode(resultpath.encode("utf-8"))
+                    result_rmv_b = encoded_string.decode('utf-8')
+                    result_rmv_b = result_rmv_b.replace('/', '_')
+                    result_rmv_b = result_rmv_b.replace('+', '-')
+                    if result_rmv_b.startswith('b'):
+                        result_rmv_b = result_rmv_b[1:]
+                    INDEX_URLVIDEO = INDEX_URL.replace(result_get_0, "0:video")
+                    share_urlvideo = INDEX_URLVIDEO + result_rmv_b
                     buttons.ubutton("🎬 Stream Link", share_urlvideo)
                     buttons.ubutton("⚡ Index Link", share_url)
                 else:
